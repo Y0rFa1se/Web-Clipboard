9e16025ac3129c577e0750ac624fdf306beb1a16
y0rfa1se1 <y0rfa1se1@debian.y0rfa1se.iptime.org>
2024-08-01 18:46:49 +0900

    1.2

--- modules/safe_md.py
+++ modules/safe_md.py
@@ -1,28 +1,42 @@
-import markdown2 as markdown
+import markdown
 import re
 
 def text2md(text):
-    return markdown.markdown(text, extensions=["tables", "footnotes", "fenced_code", "codehilite"])
+    return markdown.markdown(text) 
 
 def filter(md_content):
-    # Regex to find markdown links and images with file:// schema
     link_pattern = r'\[([^\[]+)\]\((file:\/\/\/[^\)]+)\)'
     image_pattern = r'!\[([^\[]*)\]\((file:\/\/\/[^\)]+)\)'
     html_link_pattern = r'<a\s+href="file:\/\/\/[^"]+">([^<]+)<\/a>'
     html_image_pattern = r'<img\s+src="file:\/\/\/[^"]+"[^>]*>'
 
-    # Replace local file links and images with a placeholder or empty string
-    if (re.search(link_pattern, md_content) or
+    has_local_links_or_images = (
+        re.search(link_pattern, md_content) or
         re.search(image_pattern, md_content) or
         re.search(html_link_pattern, md_content) or
-        re.search(html_image_pattern, md_content)):
-        return False
+        re.search(html_image_pattern, md_content)
+    )
 
-    return True
-
+    return not has_local_links_or_images
 
 if __name__ == "__main__":
     md_content = """
+
+    | asfd | asf |
+    | --- | --- |
+    | asf | asfd |
+    
+    ~~asdf~~
+
+    asf[^1]
+
+    [^1]: asf
+
+    ```python
+    def hello():
+        print("Hello, World!")
+    ```
+
     This is a [local file](file:///C:/Users/Example/file.txt) link.
     This is an ![local image](file:///C:/Users/Example/image.png).
     This is an <a href="file:///C:/Users/Example/file.txt">HTML local file link</a>.
@@ -31,4 +45,10 @@ if __name__ == "__main__":
     ![](https://example.com/image.png)
     """
 
-    print(filter(md_content))
+    # Convert markdown to HTML
+    html_content = text2md(md_content)
+    print(html_content)
+
+    # Check if the content passes the filter
+    print(filter(md_content))  # Should print False
+
